fad667ba51e36a3a47d4c1cae91d3b638cd0b2c5
raegon <raegon@gmail.com>
2018-10-20 21:36:06 +0900

    Fix source, target zone issue

--- src/main/java/com/raegon/pivio/media/impl/Picture.java
+++ src/main/java/com/raegon/pivio/media/impl/Picture.java
@@ -1,16 +1,16 @@
 package com.raegon.pivio.media.impl;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
 import com.raegon.pivio.media.Media;
-
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-
-import java.io.IOException;
-import java.nio.file.Path;
 
 public class Picture extends Media {
 
@@ -22,7 +22,10 @@ public class Picture extends Media {
     public DateTime getDateTime(DateTimeZone zone) {
         Metadata meta = getMetadata();
         ExifSubIFDDirectory exifDir = meta.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
-        return new DateTime(exifDir.getDateOriginal().getTime(), zone);
+        DateTime utc = new DateTime(exifDir.getDateOriginal().getTime(), DateTimeZone.UTC);
+        DateTime local = new DateTime(utc.getYear(), utc.getMonthOfYear(), utc.getDayOfMonth(),
+                utc.getHourOfDay(), utc.getMinuteOfHour(), utc.getSecondOfMinute(), zone);
+        return local;
     }
 
     private Metadata getMetadata() {
